b15749b712545bb38040f4ed05cb89444f2583a1
ntzolov <ntzolov@gmail.com>
2023-07-26 19:07:43 +0300

    Update

--- src/views/home.js
+++ src/views/home.js
@@ -1,22 +1,35 @@
 import { html } from '../lib/lit-html.js';
+import { getUser } from '../util.js';
 
-const homeTemplate = () => html`
+const homeTemplate = (userData, time) => html`
   <div class="wrapper">
     <div class="main">
-      <h1>Welcome to my notes app.</h1>
-      <h3>You can simply create and store your notes here!</h3>
-      <div class="login-register">
-        <div class="register">
-          You don't have an account? Register <a href="/register">here</a>
-        </div>
-        <div class="login">
-          You already have an account? Login <a href="/login">here</a>
-        </div>
-      </div>
+      ${userData
+        ? html`
+            <h1>Welcome, @${userData.username}</h1>
+            <p>${time}</p>
+          `
+        : html`
+            <h1>Welcome to my notes app.</h1>
+            <h3>You can simply create and store your notes here!</h3>
+            <div class="login-register">
+              <div class="register">
+                You don't have an account? Register
+                <a href="/register">here</a>
+              </div>
+              <div class="login">
+                You already have an account? Login
+                <a href="/login">here</a>
+              </div>
+            </div>
+          `}
     </div>
   </div>
 `;
 
 export async function homeView(ctx) {
-  ctx.render(homeTemplate());
+  const userData = getUser();
+  const time = new Date();
+
+  ctx.render(homeTemplate(userData, time));
 }
