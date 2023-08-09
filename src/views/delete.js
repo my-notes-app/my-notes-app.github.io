b4833cd8627c679e4124c7ef7d3015f12236477e
ntzolov <ntzolov@gmail.com>
2023-08-09 21:32:32 +0300

    update sass

--- src/views/delete.js
+++ src/views/delete.js
@@ -1,7 +1,11 @@
 import { deleteNoteById } from '../data/notes.js';
 
 export async function deleteView(ctx) {
-  const id = ctx.params.id;
-  await deleteNoteById(id);
-  ctx.page.redirect('/my-notes');
+  const confirmQuestion = confirm('Are you sure you want to delete this note?');
+
+  if (confirmQuestion) {
+    const id = ctx.params.id;
+    await deleteNoteById(id);
+    ctx.page.redirect('/my-notes');
+  }
 }
